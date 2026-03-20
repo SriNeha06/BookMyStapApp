6ef6ba00cfd072de70ff14570bed97283ab32379
SriNeha06 <srinehavanjavaka@gmail.com>
2026-03-20 09:18:10 +0530

    [SriNeha06]Added[UseCase1]

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,14 @@
+
+public class feature/UC1-WelcomePage {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" Welcome to Book My Stay Application ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=======================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay!");
+    }
+}
